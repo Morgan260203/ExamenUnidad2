0d14ba3d563b5f0b76e78361ca3b9e02e121c148
Morgan260203 <129221529+Morgan260203@users.noreply.github.com>
2023-05-22 14:18:06 -0600

    Add files via upload

---
+++ ExamenUnidad2/src/main/java/com/mycompany/examenunidad2/ExamenUnidad2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examenunidad2;
+
+/**
+ *
+ * @author Daniel
+ */
+public class ExamenUnidad2 {
+     private String nombre;
+    private String matricula;
+    private String correo;
+
+    public void Daniel68616() {
+        nombre = "Daniel Alejandro Ceh Garcia";
+        matricula = "68616";
+        correo = "al068616@uacam.mx";
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("Correo electrónico: " + correo);
+    }
+
+
+    public static void main(String[] args) {
+       ExamenUnidad2 examenunidad2 = new ExamenUnidad2();
+       examenunidad2.Daniel68616();
+    }
+}
